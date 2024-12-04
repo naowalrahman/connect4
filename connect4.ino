da8631abddf21f7ff0356735452cb47aca4cffbe
Naowal Rahman <naowal.ar@gmail.com>
2024-12-03 23:03:06 -0500

    Change to FastLED library for ws2812b leds

--- connect4.ino
+++ connect4.ino
@@ -1,12 +1,11 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
-#define PIN 6
-#define NUMPIXELS 42 // 7 columns x 6 rows
+#define LED_PIN 6
+#define NUM_LEDS 42  // 7 columns x 6 rows
 #define COLUMNS 7
 #define ROWS 6
 
-Adafruit_NeoPixel strip =
-    Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 
 // Button pins
 const int buttonPins[COLUMNS] = {2, 3, 4, 5, 6, 7, 8};
@@ -14,16 +13,17 @@ const int resetButtonPin = 9;
 
 // Board state
 int board[COLUMNS][ROWS];
-bool playerTurn = true; // true = player 1 (red), false = player 2 (yellow)
+bool playerTurn = true;  // true = player 1 (red), false = player 2 (yellow)
 
 // Colors
-uint32_t red = strip.Color(255, 0, 0);
-uint32_t yellow = strip.Color(255, 255, 0);
-uint32_t empty = strip.Color(0, 0, 0);
+CRGB red = CRGB::Red;
+CRGB yellow = CRGB::Yellow;
+CRGB empty = CRGB::Black;
 
 void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
 
   // Initialize buttons
   for (int i = 0; i < COLUMNS; i++) {
@@ -39,14 +39,14 @@ void loop() {
   for (int i = 0; i < COLUMNS; i++) {
     if (digitalRead(buttonPins[i]) == LOW) {
       dropBall(i);
-      delay(200); // Debounce delay
+      delay(200);  // Debounce delay
     }
   }
 
   // Check if reset button is pressed
   if (digitalRead(resetButtonPin) == LOW) {
     resetBoard();
-    delay(200); // Debounce delay
+    delay(200);  // Debounce delay
   }
 }
 
@@ -57,7 +57,7 @@ void dropBall(int column) {
       animateDrop(column, row);
       if (checkWin(column, row)) {
         winAnimation();
-        delay(3000); // Wait a bit before resetting
+        delay(3000);  // Wait a bit before resetting
         resetBoard();
       } else {
         playerTurn = !playerTurn;
@@ -69,23 +69,25 @@ void dropBall(int column) {
 
 void animateDrop(int column, int row) {
   for (int r = 0; r <= row; r++) {
-    strip.setPixelColor(getPixelIndex(column, r), playerTurn ? red : yellow);
-    strip.show();
-    delay(100); // Adjust for desired drop speed
+    leds[getPixelIndex(column, r)] = playerTurn ? red : yellow;
+    FastLED.show();
+    delay(100);  // Adjust for desired drop speed
     if (r < row) {
-      strip.setPixelColor(getPixelIndex(column, r), empty);
-      strip.show();
+      leds[getPixelIndex(column, r)] = empty;
+      FastLED.show();
     }
   }
 }
 
-int getPixelIndex(int column, int row) { return column + (row * COLUMNS); }
+int getPixelIndex(int column, int row) {
+  return column + (row * COLUMNS);
+}
 
 void resetBoard() {
-  for (int i = 0; i < NUMPIXELS; i++) {
-    strip.setPixelColor(i, empty);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = empty;
   }
-  strip.show();
+  FastLED.show();
 
   for (int c = 0; c < COLUMNS; c++) {
     for (int r = 0; r < ROWS; r++) {
@@ -98,18 +100,10 @@ void resetBoard() {
 
 bool checkWin(int column, int row) {
   int color = board[column][row];
-  return checkDirection(column, row, 1, 0, color) +
-                 checkDirection(column, row, -1, 0, color) >
-             2 ||
-         checkDirection(column, row, 0, 1, color) +
-                 checkDirection(column, row, 0, -1, color) >
-             2 ||
-         checkDirection(column, row, 1, 1, color) +
-                 checkDirection(column, row, -1, -1, color) >
-             2 ||
-         checkDirection(column, row, 1, -1, color) +
-                 checkDirection(column, row, -1, 1, color) >
-             2;
+  return checkDirection(column, row, 1, 0, color) + checkDirection(column, row, -1, 0, color) > 2 ||
+         checkDirection(column, row, 0, 1, color) + checkDirection(column, row, 0, -1, color) > 2 ||
+         checkDirection(column, row, 1, 1, color) + checkDirection(column, row, -1, -1, color) > 2 ||
+         checkDirection(column, row, 1, -1, color) + checkDirection(column, row, -1, 1, color) > 2;
 }
 
 int checkDirection(int column, int row, int dColumn, int dRow, int color) {
@@ -127,17 +121,13 @@ int checkDirection(int column, int row, int dColumn, int dRow, int color) {
 }
 
 void winAnimation() {
-  uint32_t color = playerTurn ? red : yellow;
+  CRGB color = playerTurn ? red : yellow;
   for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < NUMPIXELS; j++) {
-      strip.setPixelColor(j, color);
-    }
-    strip.show();
+    fill_solid(leds, NUM_LEDS, color);
+    FastLED.show();
     delay(200);
-    for (int j = 0; j < NUMPIXELS; j++) {
-      strip.setPixelColor(j, empty);
-    }
-    strip.show();
+    fill_solid(leds, NUM_LEDS, empty);
+    FastLED.show();
     delay(200);
   }
 }
